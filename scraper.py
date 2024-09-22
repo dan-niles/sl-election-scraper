060e643df03a33536662cd789e445c86f2b38580
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2024-09-22 12:25:05 +0530

    Add functionality to scraper districts and divisions

--- scraper.py
+++ scraper.py
@@ -14,6 +14,16 @@ class ElectionResult:
         return f"{self.party} - {self.candidate} - {self.votes} - {self.percentage}"
 
 
+class Division:
+    def __init__(self, name, url, status):
+        self.name = name
+        self.url = url
+        self.status = status
+
+    def to_dict(self):
+        return {"name": self.name, "url": self.url, "status": self.status}
+
+
 driver = webdriver.Chrome()
 driver.get("https://www.results.elections.gov.lk/index.php")
 
@@ -42,3 +52,43 @@ def get_all_island_results():
     # write to json
     with open("./data/all-island-results.json", "w") as f:
         json.dump([result.__dict__ for result in results], f, indent=4)
+
+
+def get_districts():
+    target_ul = driver.find_element(By.XPATH, '//*[@id="sidebar"]/ul')
+    li_elements = target_ul.find_elements(By.XPATH, "./li[position() > 2]")
+    districts = dict()
+    for i, li in enumerate(li_elements):
+        district_name = li.text
+        if district_name not in districts:
+            districts[li.text] = []
+
+    for i, district_name in enumerate(districts.keys()):
+        driver.implicitly_wait(5)
+        district_id = f"district{i+1:02}"
+
+        driver.get(
+            f"https://www.results.elections.gov.lk/district_results.php?district={district_name}"
+        )
+        h4_element = driver.find_element(
+            By.XPATH, "//h4[contains(text(), 'Polling Divisions')]"
+        )
+        target_div = h4_element.find_element(By.XPATH, "../../following-sibling::div")
+        child_elements = target_div.find_elements(By.XPATH, "./*")
+
+        for child in child_elements:
+            division_name = child.find_element(By.TAG_NAME, "p").text
+            division_link = child.get_attribute("href")
+            if division_link:
+                status = "RELEASED"
+            else:
+                status = "PENDING"
+            division = Division(division_name, division_link, status)
+            districts[district_name].append(division.to_dict())
+
+    # write to json
+    with open("./data/districts.json", "w") as f:
+        json.dump(districts, f, indent=4)
+
+
+driver.quit()
